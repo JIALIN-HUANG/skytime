06fec2e9db73f71724f1685b55843e6a92793212
JIALIN-HUANG <78190123+JIALIN-HUANG@users.noreply.github.com>
2021-04-27 15:43:31 -0700

    upload

--- script.js
+++ script.js
@@ -1,17 +1,18 @@
-const location = document.querySelectorAll("section.time.div");
+const locations = document.querySelectorAll("section.time.div")
 
-var updateTimes = function () {
-  location.forEach((location) => {
-    var outputContainer = location.querySelector("output");
-    var now = luxon.DateTime.now();
+const updateTimes = function () {
+  locations.forEach(location => {
+    const outputContainer = location.querySelector("output")
+    const timezone = location.getAttribute("data-timezone")
+    const now = luxon.DateTime.now().setZone(timezone)
 
-    outputContainer.innerHTML = now.toFormat("HH:mm:ss");
-  });
-};
+    outputContainer.innerHTML = now.toFormat("HH:mm:ss")
+  })
+}
 
-updateTimes();
+updateTimes()
 
 
 setInterval(function() {
     updateTimes()
-},1000);
+},1000)
